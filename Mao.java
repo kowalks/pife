5d0c05f3768ed7f9a0445a0ac644724072a611d5
Kowalks <guilhermekowalczuk@gmail.com>
2018-06-02 23:09:33 -0300

    Javadoc para Mao

--- Mao.java
+++ Mao.java
@@ -1,9 +1,19 @@
+/**
+ * Implementa uma classe Mao, que corresponde à mao dos jogadores
+ * que vão jogar o jogo. Ela é uma array de cartas no qual se pode
+ * comprar uma carta de um baralho ou descartar uma carta na mesa.
+ */
 public class Mao {
     private Carta[] mao;
     private Baralho mesa;
     private Baralho deck;
     private int qtdCartas;
 
+    /**
+     * Construtor padrão da classe. A mão começa sem nenhuma carta.
+     * @param mesa  o baralho para onde é descartada as cartas
+     * @param deck  o baralho de onde são compradas as cartas
+     */
     public Mao (Baralho mesa, Baralho deck)
     {
         this.mesa = mesa;
@@ -12,6 +22,10 @@ public class Mao {
         this.qtdCartas = 0;
     }
 
+    /**
+     * Compra uma carta do deck
+     * @return  true se foi possível efetuar a compra.
+     */
     public boolean compra ()
     {
         if(this.qtdCartas > 9) return false;
@@ -26,6 +40,11 @@ public class Mao {
         return true;
     }
 
+    /**
+     * Descarta uma carta na mesa
+     * @param i a posição da carta a ser descartada.
+     * @return  true se foi possível descartar a carta especificada.
+     */
     public boolean descarta(int i)
     {
         if (i < 0 || i > 10) return false;
