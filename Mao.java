de168cb91e3353bffe4b876973b632969ffc6965
Kowalks <guilhermekowalczuk@gmail.com>
2018-06-02 22:16:55 -0300

    Adicionando alguns atributos como "mesa" e "deck" na classe mão, assim é mais automatizado os métodos de compra e descarta.

--- Mao.java
+++ Mao.java
@@ -1,33 +1,43 @@
 public class Mao {
     private Carta[] mao;
+    private Baralho mesa;
+    private Baralho deck;
     private int qtdCartas;
 
-    public Mao ()
+    public Mao (Baralho mesa, Baralho deck)
     {
+        this.mesa = mesa;
+        this.deck = deck;
         this.mao = new Carta[10];
         this.qtdCartas = 0;
     }
 
-    public boolean recebe(Carta carta)
+    public boolean compra ()
     {
+        if(this.qtdCartas > 9) return false;
+
+        Carta carta = deck.compra();
+
         // sanity check
-        if (carta == null || this.qtdCartas > 9) return false;
+        if (carta == null) return false;
 
         this.mao[this.qtdCartas] = carta;
         this.qtdCartas++;
         return true;
     }
 
-    public Carta retira(int i)
+    public boolean descarta(int i)
     {
-        if (i < 0 || i > 10) return null;
-        if (mao[i] == null) return null;
+        if (i < 0 || i > 10) return false;
+        if (mao[i] == null) return false;
 
         Carta cartaAux = mao[i];
         for (int k = i; k < qtdCartas - 1; k++)
         {
             mao[k] = mao[k+1];
         }
-        return cartaAux;
+        mesa.descarta(cartaAux);
+
+        return true;
     }
 }
