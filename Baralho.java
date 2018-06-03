30aed374a764e6d02c49f1f50a62b7a34c1adb97
Kowalks <guilhermekowalczuk@gmail.com>
2018-06-02 23:05:04 -0300

    Javadoc para a classe baralho

--- Baralho.java
+++ Baralho.java
@@ -1,9 +1,9 @@
 import java.util.Random;
 /**
- * Escreva a descrição da classe Baralho aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
+ * Implementa um baralho, constituido por uma array de cartas (classe Carta)
+ *
+ * @author Guilherme Kowalczuk
+ * @version 02/06/2018
  */
 public class Baralho
 {
@@ -11,13 +11,23 @@ public class Baralho
     private int finalBaralho; // esse aponta para o indice do ultimo elemento do baralho
     
     private static int numCartas = 52;
-    
+
+    /**
+     * Construtor padrão da classe. Começa com uma array em que todos os
+     * elementos são nulos, e o final do baralho aponto para a posição -1.
+     */
     public Baralho ()
     {
         this.deck = new Carta[numCartas];
         this.finalBaralho = -1;
     }
-    
+
+    /**
+     * Inicializa o baralho preenchendo com todas as cartas de um balhalho convencional,
+     * isto é, inicializa com 13 cartas de números (A, 2, ..., 10, J, Q, K) de 4 naipes
+     * diferentes (copas, paus, espadas, ouros). Ao final, o baralho é embaralhado e
+     * suas cartas são misturadas.
+     */
     public void inicializa()
     {
         this.finalBaralho = numCartas - 1;
@@ -57,7 +67,11 @@ public class Baralho
         } while (t == 1);
         
     } */
-    
+
+    /**
+     * Embaralha o baralho, isto é, deixa as cartas randomicamente organizadas
+     * no baralho.
+     */
     public void embaralha()
     {
         Random gera = new Random();
@@ -69,14 +83,26 @@ public class Baralho
             swapCartas(rand1, rand2);
         }
     }
-    
+
+    /**
+     * Troca duas cartas em uma certa posição do baralho de lugar. Método privado
+     * usado no método de embaralhamento.
+     * @param i primeira posição a ser considerada
+     * @param j segunda posição a ser considerada
+     */
     private void swapCartas(int i, int j)
     {
         Carta aux = deck[i];
         deck[i] = deck[j];
         deck[j] = aux;
     }
-    
+
+    /**
+     * Tira a carta do topo do baralho. O espaço que essa carta estava ocupando é
+     * preenchido com vazio, então se pode realmente considerar que a carta foi removida
+     * @return o endereço da carta removida. Note que o baralho é estruturalmente uma
+     * pilha, então essa é a carta do topo do baralho.
+     */
     public Carta compra()
     {
         if (finalBaralho < 0) return null;
@@ -85,10 +111,17 @@ public class Baralho
         this.finalBaralho--;
         return carta;
     }
-    
+
+    /**
+     * Acrescenta a carta no baralho. Note que o baralho é uma pilha, e essa carta é
+     * organizada na parte de cima do baralho.
+     * @param carta a carta a ser acrescentada.
+     */
     public void descarta(Carta carta)
     {
         this.finalBaralho++;
         this.deck[finalBaralho] = carta;
     }
+
+
 }
