e5e2893704d668169ac2472c1a361ef14dc3aaf7
Kowalks <guilhermekowalczuk@gmail.com>
2018-06-02 22:33:56 -0300

    Método de inicialização do jogo.[

--- Jogo.java
+++ Jogo.java
@@ -2,13 +2,26 @@ public class Jogo {
     private int numJogadores;
     private Baralho mesa;
     private Baralho deck;
+    private Mao[] jogador;
 
     public Jogo (int numJogadores)
     {
-        this.numJogadores = numJogadores;
-        mesa = new Baralho ();
-        deck = new Baralho ();
+        if (numJogadores > 1 && numJogadores < 5) {
+            this.numJogadores = numJogadores;
+            mesa = new Baralho();
+            deck = new Baralho();
+            jogador = new Mao[numJogadores];
+        }
+    }
+
+    private void inicializa()
+    {
         deck.inicializa();
+        for (int i = 0; i < numJogadores; i++) {
+            jogador[i] = new Mao(this.mesa, this.deck);
+            for (int j = 0; j < 9; j++)
+                jogador[i].compra();
+        }
     }
 
 }
